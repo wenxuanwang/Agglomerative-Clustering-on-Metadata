75928ea4819dda325783eaeefe429d55fb19a139
Wenxuan Wang <282636128@qq.com>
2017-02-09 20:42:27 -0500

    Update cluster_and_count.py

--- cluster_and_count.py
+++ cluster_and_count.py
@@ -101,14 +101,3 @@ if __name__ == "__main__":
     print "Size:",len(cluster)
     print "Number of Users:",len(users)
     print "Number of Groups:",len(groups)
-        
-        
-      
-    
-    
-    
-    
-    
-  
-    
-    
